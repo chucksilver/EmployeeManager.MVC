192658e7b2af12dd3b16e49dec01e9aa337765f8
Charles Plata <charles.plata@gmail.com>
2020-04-12 21:11:55 -0400

    test

--- EmployeeManager.MVC/EmployeeManager.MVC/Models/Employee.cs
+++ EmployeeManager.MVC/EmployeeManager.MVC/Models/Employee.cs
@@ -10,5 +10,43 @@ namespace EmployeeManager.MVC.Models
     [Table("Employee")]
     public class Employee
     {
+        [Column("EmployeeID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "Employee ID is required")]
+        [Display(Name = "Employee ID")]
+        public int EmployeeId { get; set; }
+        [Column("FirstName")]
+        [Display(Name ="First Name")]
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(10,ErrorMessage ="First Name must be less than 10 characters")]
+        public string FirstName { get; set; }
+        [Column("LastName")]
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(10, ErrorMessage = "Last Name must be less than 20 characters")]
+        public string LastName { get; set; }
+        [Column("Title")]
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(10, ErrorMessage = "Title must be less than 30 characters")]
+        public string Title { get; set; }
+        [Column("BirthDate")]
+        [Display(Name = "Birth Date")]
+        [Required(ErrorMessage ="Birth Date is required")]
+        public DateTime BirthDate {get;set;}
+        [Column("HireDate")]
+        [Display(Name ="HIre Date")]
+        [Required(ErrorMessage ="Hired Date is requried")]
+        public DateTime HireDate { get; set; }
+        [Column("Country")]
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(15, ErrorMessage = "Country must be less than 15 characters")]
+        public string Country { get; set; }
+        [Column("Notes")]
+        [Display(Name ="Notes")]
+        [StringLength(500, ErrorMessage = "Note must be less than 500 characters")]
+        public string Note { get; set; }
     }
 }
